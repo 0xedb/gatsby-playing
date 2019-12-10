68320d223569805f985fcc72a849242dcffebe5b
bruno edoh <edohbruno@gmail.com>
2019-12-10 12:41:05 +0000

    Initial commit

---
+++ src/components/fancy.js
@@ -0,0 +1,8 @@
+import React from "react"
+import {bd} from '../style/fancy.module.css';
+
+function Fancy() {
+  return <h3 className={bd}>working</h3>
+}
+
+export default Fancy
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Fancy from "../components/fancy"
+
+export default () => (
+  <div>
+     word!
+    <Fancy />!
+  </div>
+)
+    
